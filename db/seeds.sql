1672d98d44cc143c54f588d5cb7e7e9e64cdfbce
KakaliKobal <demonaddicted@outlook.com>
2018-01-09 22:41:38 -0700

    set up

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Plain Hamburger', false, NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Roast Beef and Cheddar', false, NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('BLT Burger', false, NOW());
+
